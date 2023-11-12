9b3a6692fe01abdf947ab74cb2225fa9679b5d3f
Jay-SCM <John.Farmer1086@gmail.com>
2023-11-12 23:02:50 +0930

    changed button size added button animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////                MirukuSCM  WeatherAPP              ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    const [city, setCity] = useState('');
+    const [weather, setWeather] = useState(null);
+    const [error, setError] = useState('');
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const fetchWeather = async () => {
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            setWeather(response.data);
+            setError('');
+        } catch (error) {
+            setWeather(null);
+            setError('City not found. Please try again.');}};
+    return (
+        <div className="weather-app">
+            <video autoPlay loop muted className="video-background">
+                <source src="/video/Video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className="weather-card">
+                <h1>GawsedrftG</h1>
+                <input
+                    type="text"
+                    placeholder="Enter city"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}/>
+                <button onClick={fetchWeather}>Get Weather</button>
+                {weather && (
+                    <div className="weather-info">
+                        <h2>{weather.name}, {weather.sys.country}</h2>
+                        <p>{weather.weather[0].description}</p>
+                        <p>Temperature: {weather.main.temp}°C</p>
+                        <p>Humidity: {weather.main.humidity}%</p>
+                    </div>)}
+                {error && <p className="error">{error}</p>}
+            </div>
+        </div>);}
 export default App;
+
