e6686063d2449f71870ca747f1eb4f90ea7bbe88
Jay-SCM <John.Farmer1086@gmail.com>
2023-11-12 23:30:36 +0930

    added navbar and links

--- src/App.js
+++ src/App.js
@@ -8,26 +8,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
-
 function App() {
     const [city, setCity] = useState('');
     const [weather, setWeather] = useState(null);
     const [error, setError] = useState('');
     const API_KEY = process.env.REACT_APP_API_KEY;
-
     const fetchWeather = async () => {
         try {
             const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-            );
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
             setWeather(response.data);
             setError('');
         } catch (error) {
             setWeather(null);
-            setError('City not found. Please try again.');
-        }
-    };
-
+            setError('City not found. Please try again.');}};
     return (
         <div className="weather-app">
             <nav className="navbar">
@@ -51,8 +45,7 @@ function App() {
                     type="text"
                     placeholder="Enter city"
                     value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                />
+                    onChange={(e) => setCity(e.target.value)}/>
                 <button onClick={fetchWeather}>Search</button>
                 {weather && (
                     <div className="weather-info">
@@ -60,14 +53,10 @@ function App() {
                         <p>{weather.weather[0].description}</p>
                         <p>Temperature: {weather.main.temp}°C</p>
                         <p>Humidity: {weather.main.humidity}%</p>
-                    </div>
-                )}
+                    </div>)}
                 {error && <p className="error">{error}</p>}
             </div>
-        </div>
-    );
-}
-
+        </div>);}
 export default App;
 
 
